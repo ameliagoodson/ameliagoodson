56ee723405caa4bd612178d28baa20c911c143b7
Amelia Goodson <ameliajanegoodson@gmail.com>
2024-07-17 21:33:46 -0400

    Build version of curvy block

--- app/web/app/plugins/custom-blocks/build/blocks/curvy/index.asset.php
+++ app/web/app/plugins/custom-blocks/build/blocks/curvy/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'e3c3162dcb0f59cc1acb');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '28bf02f166e35b3a02cb');
