df2be3db741dd6b0c1f58eb6b96bd2921c005cb5
Amelia Goodson <ameliajanegoodson@gmail.com>
2024-07-25 13:46:39 -0400

    Initial mobile menu

--- app/web/app/themes/ameliagoodson/parts/global/site-header.php
+++ app/web/app/themes/ameliagoodson/parts/global/site-header.php
@@ -10,7 +10,7 @@ $header_color = get_theme_mod('header_color');
 ?>
 <header id="site-header" <?php if ($header_color) : ?> style="background-color: <?php echo $header_color;
                                                                               endif; ?>">
-  <div class="header-inner-wrapper">
+  <div class="header-inner-wrapper d-desktop">
     <nav class="header-inner section-inner">
       <h1 class="site-logo">
         <a href="<?php echo esc_url(home_url()) ?>"><img src="http://ameliagoodson.local/app/uploads/2024/06/logo-brand-black-future.png"></a>
@@ -23,4 +23,16 @@ $header_color = get_theme_mod('header_color');
       ?>
     </nav>
   </div>
+  <div class="mobile-header d-mobile">
+    <button class="hamburger-btn" aria-controls="mobile-menu" aria-expanded="false">
+      <div class="menu-bar top"></div>
+      <div class="menu-bar middle"></div>
+      <div class="menu-bar bottom"></div>
+    </button>
+    <nav class="mobile-menu-container">
+      <?php wp_nav_menu(array(
+        'theme_location' => 'primary',
+        'menu_class' => 'mobile-menu',
+      )) ?>
+    </nav>
 </header>
