192905948cf4cb0a026f34b9534a901cce48485c
Amelia Goodson <ameliajanegoodson@gmail.com>
2024-08-28 13:21:39 -0400

    Fix grid layout

--- web/app/themes/ameliagoodson/assets/js/scripts-ag.js
+++ web/app/themes/ameliagoodson/assets/js/scripts-ag.js
@@ -1,4 +1,77 @@
 /* ------------------------------------------------------------------------------ /*
+/*  NAMESPACE
+/* ------------------------------------------------------------------------------ */
+
+var agtheme = agtheme || {},
+  $ = jQuery;
+
+/* ------------------------------------------------------------------------------ /*
+/*  GLOBALS
+/* ------------------------------------------------------------------------------ */
+
+var $agthemeDoc = $(document),
+  $agthemeWin = $(window),
+  agthemeIsIE11 = !!window.MSInputMethodContext && !!document.documentMode;
+
+/* ------------------------------------------------------------------------------ /*
+/*  GRID
+/* ------------------------------------------------------------------------------ */
+
+agtheme.grid = {
+  init: function () {
+    var $wrapper = $(".posts-grid");
+
+    if ($wrapper.length) {
+      $wrapper.imagesLoaded(function () {
+        $grid = $wrapper.isotope({
+          columnWidth: ".grid-sizer",
+          itemSelector: ".js-grid-item",
+          percentPosition: true,
+          stagger: 0,
+          transitionDuration: "0.25s",
+          hiddenStyle: { opacity: 0 },
+          visibleStyle: { opacity: 1 },
+          layoutMode: $wrapper.data("layout") == "masonry" ? "masonry" : "fitRows",
+        });
+
+        // Trigger will-be-spotted elements.
+        $grid.on("layoutComplete", function () {
+          $agthemeWin.trigger("scroll");
+        });
+
+        // Check for Masonry layout changes on an interval. Accounts for DOM changes caused by lazyloading plugins.
+        // The interval is cleared when all previews have been spotted.
+        agtheme.grid.intervalUpdate($grid);
+
+        // Reinstate the interval when new content is loaded.
+        $agthemeWin.on("ajax-content-loaded", function () {
+          agtheme.grid.intervalUpdate($grid);
+        });
+      });
+    }
+  },
+
+  intervalUpdate: function ($grid) {
+    var gridLayoutInterval = setInterval(function () {
+      $grid.isotope();
+
+      // Clear the interval when all previews have been spotted.
+      if (!$(".preview.do-spot:not(.spotted)").length) {
+        clearInterval(gridLayoutInterval);
+      }
+    }, 1000);
+  },
+};
+
+/* ------------------------------------------------------------------------------ /*
+/*  INIT
+/* ------------------------------------------------------------------------------ */
+
+$agthemeDoc.ready(function () {
+  agtheme.grid.init();
+});
+
+/* ------------------------------------------------------------------------------ /*
 /*  SCROLL REVEAL JS
 /* ------------------------------------------------------------------------------ */
 
@@ -47,7 +120,3 @@ document.addEventListener("DOMContentLoaded", function () {
     delay: 400,
   });
 });
-
-/* ------------------------------------------------------------------------------ /*
-/*  NAVIGATION TOGGLE 
-/* ------------------------------------------------------------------------------ */
